8d499e14382a784866f942c8636012ba4f23bed1
tghelere <ghelere@outlook.com>
2017-08-17 15:29:25 -0300

    install cors middleware and pre configure with access for all (*)

---
+++ src/server/cors.js
@@ -0,0 +1,10 @@
+const corsMiddleware = require('restify-cors-middleware')
+
+const cors = corsMiddleware({
+    preflightMaxAge: 5,
+    origins: ['*'],
+    allowHeaders: ['*'],
+    exposeHeaders: ['*']
+})
+
+module.exports = cors;
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
 const restify = require('restify');
 const server = restify.createServer();
 const routes = require('../http/routes');
+const cors = require('./cors');
+
+server.pre(cors.preflight)
+server.use(cors.actual)
 
 routes(server);
 
